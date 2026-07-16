15bb80d81f7b84fe5af0cba722b908f26f3aa0ca
ikuto-yamaguchi <75665201+ikuto-yamaguchi@users.noreply.github.com>
2026-07-17 07:09:54 +0900

    HS18 V3: use final generalized causal runtime

--- minimal-predictive-lm/src/minimal_predictive_lm/sparc_hs18_worker_v3.py
+++ minimal-predictive-lm/src/minimal_predictive_lm/sparc_hs18_worker_v3.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 
 from . import sparc_hs18_worker as _worker
-from .english_causal_compiler_v3 import EnglishCausalResolverV3
+from .english_causal_compiler_v3_runtime import EnglishCausalResolverV3Runtime
 
-_worker.EnglishCausalResolver = EnglishCausalResolverV3
+_worker.EnglishCausalResolver = EnglishCausalResolverV3Runtime
 main = _worker.main
 
 
